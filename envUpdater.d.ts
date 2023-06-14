c3b9a14176732e250d0ee7a028d31c4477707601
Sanjula Ganepola <Sanjula.Ganepola@ibm.com>
2023-06-14 11:47:50 -0400

    update types

--- envUpdater.d.ts
+++ envUpdater.d.ts
@@ -1,3 +1,3 @@
 import { Uri } from "vscode";
 import { EnvironmentVariables } from "./iproject";
-export default function envUpdater(envUri: Uri, variables: EnvironmentVariables): Promise<void>;
+export default function envUpdater(envUri: Uri, variables: EnvironmentVariables): Promise<boolean>;
